78e359b4cdd9ba73da8844e8b5413991f2673485
shleexd <shlee_xd@163.com>
2016-06-16 18:48:04 +0800

    update Bulk_quote class

--- Chapter15/bulk_quote.h
+++ Chapter15/bulk_quote.h
@@ -8,7 +8,13 @@ class Bulk_quote : public Quote
 {
 public:
     Bulk_quote() = default;
+    Bulk_quote( const std::string &book, double sales_price,
+                std::size_t qty, double disc) :
+        Quote(book, sales_price), min_qty(qty), discount(disc) { }
     double net_price(std::size_t n) const override;
+private:
+    std::size_t min_qty;
+    double discount;
 };
 
 #endif // BULK_QUOTE_H
