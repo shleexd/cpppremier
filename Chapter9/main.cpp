c376add5758e06ac105f4c47ec5849526e475b95
shleexd <shlee_xd@163.com>
2016-06-25 23:29:44 +0800

    update ch9

--- Chapter9/main.cpp
+++ Chapter9/main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <list>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int main(int argc, char *argv[])
+void exc_9_13(void){
+    list<int> ls_int = { 32, 43, 53, 32, 43};
+
+    //vector<double> vec_dbl(ls_int.cbegin(), ls_int.cend());
+    vector<double> vec_dbl;
+    vec_dbl.assign(ls_int.cbegin(), ls_int.cend() );
+
+    for(auto &i : vec_dbl){
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main(void)
 {
-    cout << "Hello World!" << endl;
+    exc_9_13();
     return 0;
 }
