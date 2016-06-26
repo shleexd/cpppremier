d34a559fdfd68c2f6edc406b2d0a185299a96c23
shleexd <shlee_xd@163.com>
2016-06-26 17:44:43 +0800

    update ch9

--- Chapter9/main.cpp
+++ Chapter9/main.cpp
@@ -11,6 +11,12 @@ void exc_9_13(void){
     //vector<double> vec_dbl(ls_int.cbegin(), ls_int.cend());
     vector<double> vec_dbl;
     vec_dbl.assign(ls_int.cbegin(), ls_int.cend() );
+    vec_dbl.emplace_back(32);
+    //it would be slow to insert to any place expect end of a vector.
+    vec_dbl.insert(vec_dbl.begin(), 22);
+    vec_dbl.insert(vec_dbl.end(), 43, 6);
+    vec_dbl.erase(vec_dbl.begin() + 3, vec_dbl.end());
+    vec_dbl.resize(1);
 
     for(auto &i : vec_dbl){
         cout << i << " ";
